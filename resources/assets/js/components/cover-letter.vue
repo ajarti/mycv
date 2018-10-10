0528c4b97ddd37540d5fc41e92b03b28fc5aff88
David Murray <david.murray@ajarti.com>
2018-10-10 11:40:33 +0200

    Final Build.

--- resources/assets/js/components/cover-letter.vue
+++ resources/assets/js/components/cover-letter.vue
@@ -65,33 +65,33 @@
                         </p>
 
                         <p>
-                            I am a Full Stack Developer with ~20 years development experience. I currently specialise in Laravel coupled primarily with Vue on the frontend.
+                            I am a Full Stack Developer with ~20 years of development experience. I currently specialise in Laravel coupled primarily with Vue on the frontend.
                         </p>
                         <p>
                             I have 30+ years IT experience in total (25 commercial) having started at the early age of 9 as your typical nerd doing what he can to get games to work.
                         </p>
                         <p>
-                            In my career thus far, I have done everything from building computers/servers/clusters from bare metal and managing them, designing and constructing networks(to WAN level) and for the last 20 years, full stack online development utilizing cutting edge backend/frontend frameworks.
+                            In my career thus far, I have done everything from building computers/servers/clusters from bare metal and managing them, designing and constructing networks(to WAN level) and for the last 20 years, full stack online development utilizing cutting-edge backend/frontend frameworks.
                         </p>
                         <p>
                             I owned and run a Digital Agency for 10 years and have an intricate understanding of client interaction from initial strategy to managing development and delivery, having said this my preference is development.
                         </p>
                         <p>
-                            I have a deep understanding of the internet, it's core components and how everything hangs together. I have witnessed the Internet's evolution from the early ‘90s and as a result I am able to select technologies and strategies that match the client’s needs and help maximize the project's probability of success.
+                            I have a deep understanding of the internet, it's core components and how everything hangs together. I have witnessed the Internet's evolution from the early ‘90s and as a result, I am able to select technologies and strategies that match the client’s needs and help maximize the project's probability of success.
                             I pride myself on avoiding fads and ensuring that the correct strategy is selected, utilising the right tools that fit both the budget and fulfil the requirement.
                             I am a fan of <a href="http://theleanstartup.com/" target="_blank" class="link-fx text-warning">The Lean Startup</a> methodology and strongly believe in small iterations to gauge feasibility as well as helping manage client expectations and budgets. I am also a huge fan of Basecamp's <a href="https://basecamp.com/books/rework" target="_blank" class="link-fx text-warning">Rework</a> principles.
                         </p>
                         <p>
-                            I found your advert very appealing as I feel very strongly about work life balance. Balance was the driving force behind leaving my agency when my son was born, I had lost contact with my love of development and was spending all my time in management. I think well rested and happy developers are the most focused, productive and creative. I love to collaborate and share knowledge and believe in finding wonder in whatever you do.
+                            I found your advert very appealing as I feel very strongly about work-life balance. Balance was the driving force behind leaving my agency when my son was born, I had lost contact with my love of development and was spending all my time in management. I think well rested and happy developers are the most focused, productive and creative. I love to collaborate and share knowledge and believe in finding wonder in whatever you do.
                         </p>
                         <p>
-                            I am looking to relocate with my family to the UK or possibly Central Europe and with the current political situation and constant economic downgrades by the ratings agencies, our currency has degraded to the point where the exchange rate would not allow me to do this easily. This is why I am now looking for an income source from a International base while retaining my independence.
+                            I am looking to relocate with my family to the UK or possibly Central Europe and with the current political situation and constant economic downgrades by the Rating Agencies, our currency has degraded to the point where the exchange rate would not allow me to do this easily. This is why I am now looking for an income source from an International base while retaining my independence.
                         </p>
                         <p>
                             I am a native English speaker living in Cape Town (South Africa) UTC+2.
                         </p>
                         <p>
-                            Please feel free to review my <router-link to="/resume" class="link-fx text-warning">resume</router-link> which is part of this simple SPA and contains more detail on my employment history and skill set. Most of my recent code is closed source, so I developed this resume as a example of a Lavavel/Vue app. It’s deployed live to a Digitalocean server via Forge. If you would like to review the source code, please send me an email address that I can add to the collaborators of this project.
+                            Please feel free to review my <router-link to="/resume" class="link-fx text-warning">resume</router-link> which is part of this simple SPA and contains more detail on my employment history and skill set. Most of my recent code is closed source, so I developed this resume as an example of a Lavavel/Vue app. It’s deployed live to a Digitalocean server via Forge. If you would like to review the source code, please send me an email address that I can add to the collaborators of this project.
                         </p>
                         <div>Yours sincerely,</div>
                         <div class="my-2 d-inline-block">
